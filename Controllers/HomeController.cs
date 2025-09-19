c20e252c672651bc71e285f4f3997696c049db80
Max <95097703@est.ort.edu.ar>
2025-09-19 09:02:35 -0300

    Terminar clase Juego, Terminar Controller, Empezando a Sessionar todo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,4 +17,35 @@ public class HomeController : Controller
     {
         return View();
     }
+     public IActionResult ConfigurarJuego()
+    {
+    list<Categoria> categorias = Juego.ObtenerCategorias();
+    list<Dificultades> dificultades = Juego.ObtenerDificultades();
+    ViewBag.Categorias = categorias;
+    categorias = Objeto.StringToList<Categoria> (HttpContext.Session.GetString("categorias"));
+    //Falta crear StringToList
+    ViewBag.Dificultades = dificultades;
+    return View("ConfigurarJuego");
+    }
+    public IActionResult Comenzar(string username, int dificultad, int categoria){
+        ViewBag.ListaPreguntas = Juego.CargarPartida(username,dificultad,categoria);
+        ViewBag.username = username;
+        return View("Jugar");
+    }
+    
+    public IActionResult Jugar() {
+        ViewBag.Pregunta = Juego.ObtenerProximaPregunta();
+        if (ViewBag.Pregunta == null) {
+            return View("Fin");
+        }
+        else {
+            ViewBag.Respuestas = Juego.ObtenerProximasRespuestas(ViewBag.Pregunta.IdPregunta);
+            return View("Juego");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult VerificarRespuesta(int idPregunta, int idRespuesta) {
+        ViewBag.esCorrecto = Juego.VerificarRespuesta(idRespuesta);
+    }
 }
--- Models/Juego.cs
+++ Models/Juego.cs
@@ -1,5 +1,5 @@
 public class Juego{
-    string username{get; set;}
+    string username {get; set;}
     int PuntajeActual {get; set;}
     int CantidadPreguntasCorrectas{get; set;}
     int ContadorNroPreguntaActual{get; set;}
@@ -17,15 +17,41 @@ public class Juego{
         ListaRespuestas = null;
     }
 
-public List<Categoria> ObtenerCategorias(){
-            return BD.ObtenerCategorias();
+    public List<Categoria> ObtenerCategorias(){
+                return BD.ObtenerCategorias();
+            }
+    public List<Dificultad> ObtenerDificultades(){
+                return BD.ObtenerDificultades();
+    }
+    public List<Pregunta> CargarPartida(string username, int dificultad, int categoria){
+        InicializarJuego();
+        List<Pregunta> listaPreguntas = BD.ObtenerPreguntas(dificultad,categoria);
+        return listaPreguntas;
+    }
+    public Pregunta ObtenerProximaPregunta()
+    {
+        if (ContadorNroPreguntaActual + 1 < ListaPreguntas.Count)
+        {
+            return ListaPreguntas[ContadorNroPreguntaActual + 1];
         }
-public List<Dificultad> ObtenerDificultades(){
-            return BD.ObtenerDificultades();
-}
-public List<Pregunta> CargarPartida(string username, int dificultad, int categoria){
-    InicializarJuego();
-    List<Pregunta> listaPreguntas = BD.ObtenerPreguntas(dificultad,categoria);
-    return listaPreguntas;
-}
+        return null;
+    }
+    public List<Respuesta> ObtenerProximasRespuestas(int idPregunta) {
+        List<Respuesta> respuestas = BD.ObtenerRespuestas(idPregunta);
+        return respuestas;
+    }
+    public bool VerificarRespuesta(int idRespuesta) {
+        bool esCorrecto;
+        if (idRespuesta == ListaRespuestas[ContadorNroPreguntaActual].IdRespuesta && ListaRespuestas[ContadorNroPreguntaActual].Correcta == true) {
+            esCorrecto = true;
+            PuntajeActual += 1000;
+            CantidadPreguntasCorrectas += 1;
+        }
+        else {
+            esCorrecto = false;
+        }
+        ContadorNroPreguntaActual += 1;
+        ObtenerProximaPregunta();
+        return esCorrecto;
+    }
 }
