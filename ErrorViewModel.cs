298d5d3bfccd7bf61a13ef9008ac47ebde258fcb
Max <95097703@est.ort.edu.ar>
2025-10-08 21:37:31 -0300

    Borrar archivos innecesarios

--- ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TP08_Ku_Hevia.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
